48dc70211ebfb4c514f77ddd73b0d2be795b6550
mattiabenelli <benellimattia0@gmail.com>
2022-12-13 18:32:51 +0100

    milestone 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-alert("a")
+let team = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        img: "wayne-barnet-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Chief",
+        img: "angela-caroll-chief-chief.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    }
+]
+console.log(team)
